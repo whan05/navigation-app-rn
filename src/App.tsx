6cc496a58286f24d389036c44ba112ef7929c0a2
whan05 <wfonsecap-m@ucenfotec.ac.cr>
2025-07-16 22:01:57 -0600

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { Text, View } from 'react-native';
+
+export const App = () =>  {
+
+  return (
+    <View>
+      <Text>Hola Mundo</Text>
+    </View>
+  );
+}
+
---
+++ src/presentation/theme/theme.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export const globalColors = {
+    
+}
+
+
+export const globalStyles = StyleSheet.create({
+
+}) 
